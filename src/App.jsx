ca5109e5d331c337e23dd69d2524fcd79712d3d8
Justina18 <igbanijustina@gmail.com>
2023-01-25 14:59:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,106 @@
+import React,{useState} from 'react'
+import Form from './Form';
+import './App.css'
+
+const App = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    address:"",
+    password: "",
+    confirmPassword: "",
+  })
+
+  const inputs=[
+    {
+    id:1,
+    name:'name',
+    placeholder: 'Name',
+    type: 'text',
+    required: true,
+    errMsg:'All characters must be letters. There should be at least 3 characters ',
+    pattern: `[a-zA-Z][a-zA-Z0-9-_. ]{3,20}`
+  },
+  {
+    id:2,
+    name:'email',
+    placeholder: 'E-mail',
+    type: 'email',
+    required: true,
+    errMsg:'Must be a valid e-mail',
+    pattern:`^\S+@\S+$`
+  },
+  {
+    id:3,
+    name:'address',
+    placeholder: 'Address',
+    type: 'address',
+    required: true,
+
+  },
+  {
+    id:4,
+    name:'password',
+    placeholder: 'Password',
+    type: 'password',
+    required: true,
+    errMsg:'There must be at least 8 characters. It must have a capital letter, a number, a special character and small letters',
+    pattern:`^(?=.?[A-Z])(?=.?[a-z])(?=.?[0-9])(?=.?[#?!@$%^&*-]).{8,}$`
+  },
+  {
+    id:5,
+    name:'password',
+    placeholder: 'Confirm Password',
+    type: 'password',
+    required: true,
+    errMsg:'Must match the password',
+    pattern: values.password
+  },
+];
+
+const handleChange= e =>{
+  setValues({...values, [e.target.name]: e.target.values});
+}
+
+const receivedValues =(e)=>{
+  e.preventDefault()
+  console.log(values)
+  window.location.reload()
+}
+
+// const [showPassword, setShowPassword] = useState(false);
+// const Visible = () => {
+//   setShowPassword(prevState => !prevState);
+// };
+// const [toggle, setToggle] = useState(true);
+// const handleToggle = () => { setToggle(!toggle) }
+//   const openEye = (<AiOutlineEye fontSize={18} color="black" onClick={handleToggle} />)
+//   const closedEye = (<div>
+//       <RiEyeCloseLine fontSize={18} color="black" onClick={handleToggle} />
+//                 </div>)
+
+  return (
+    <div>
+        <div className='App'>
+          <form onSubmit={receivedValues}>
+            <h1>Sign Up</h1>
+          {inputs.map((i)=>
+      <Form key={i.id} {...i} handleChange={handleChange} values={values[i.name]}/>
+      )}
+            <div className='foot'>
+              <div>
+              <input type="checkbox" name="terms" id="terms" />
+            <label className='label' for="terms">I have read and agreed to the <b className='b'> Terms and conditions</b></label>
+            </div>
+            <button className='button' type='submit'>
+                CREATE ACCOUNT
+            </button>
+            <p className='p'>Already have an account? <b className='b'> Log In.</b></p>
+            </div>
+          </form>
+        </div>
+    </div>
+  )
+}
+
+export default App;
